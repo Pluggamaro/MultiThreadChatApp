ecfcca6a7767bc82ca552ac7a4ede2650e52898f
Bongani X <velanibzuma@gmail.com>
2025-09-02 00:33:05 +0200

    -Client Handle implements Runnable and overrides the run method

--- src/ClientHandle.java
+++ src/ClientHandle.java
@@ -2,10 +2,21 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientHandle {
+public class ClientHandle implements Runnable {
     private final Socket clientSocket;
     private final AppServer server;
     private PrintWriter out;
     private BufferedReader in;
     private String user;
+
+    public ClientHandle(Socket socket, AppServer server){
+        this.clientSocket = socket;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        
+    }
+
 }
