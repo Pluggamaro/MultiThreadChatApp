642b374736dee3026fbd6c9ca62be0d6b111ac69
Bongani X <velanibzuma@gmail.com>
2025-09-02 00:15:51 +0200

    -in start, in Appserver, communicating connection success

--- src/AppServer.java
+++ src/AppServer.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -7,7 +9,15 @@ public class AppServer {
     private final Set<ClientHandle> clientHandler = new CopyOnWriteArraySet<>();
     
     public void start(){
-        
+        //informing of port number
+        System.out.println("Server started on port: " + PORT);
+        try(ServerSocket socket = new ServerSocket(PORT)){
+            System.out.println("Server start success! Awaiting connections...");
+            
+
+        }catch(IOException w){
+            System.err.println("Server error: " + w.getMessage());
+        }
     }
 
 }
