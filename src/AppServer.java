2d1bdacc34c385c38916d77e088635ae242decd5
Bongani X <velanibzuma@gmail.com>
2025-09-02 00:11:02 +0200

    -defining start method in AppServer

--- src/AppServer.java
+++ src/AppServer.java
@@ -1,4 +1,13 @@
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 public class AppServer {
     private static final int PORT = 9090;
+
+    private final Set<ClientHandle> clientHandler = new CopyOnWriteArraySet<>();
     
+    public void start(){
+        
+    }
+
 }
