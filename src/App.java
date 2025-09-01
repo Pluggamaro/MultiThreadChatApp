e0438776932ebdb48e5dae1e80bb5829622d851b
Bongani X <velanibzuma@gmail.com>
2025-09-02 01:12:59 +0200

    -outlining main interface

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
 
-    
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 9090;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        try{
+            
+        }catch
+
     }
 }
