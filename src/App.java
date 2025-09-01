ba7e72f24e5069be14b52ac7293a1b92e8cb8b39
Bongani X <velanibzuma@gmail.com>
2025-09-02 01:18:31 +0200

    -outlining outer try-catch clause in main

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class App {
 
     private static final String SERVER_ADDRESS = "127.0.0.1";
@@ -5,8 +10,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         try{
+            Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader in = new BufferedReader((new InputStreamReader(socket.getInputStream())));
             
-        }catch
+
+        }catch(){
+
+        }
 
     }
 }
