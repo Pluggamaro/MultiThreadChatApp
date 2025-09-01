750263e3a1651076a79ade20e606fce4e2d57886
Bongani X <velanibzuma@gmail.com>
2025-09-02 01:25:50 +0200

    -starting server in main and multiple threads

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -9,10 +10,29 @@ public class App {
     private static final int SERVER_PORT = 9090;
     public static void main(String[] args) throws Exception {
         
-        try{
+        try(
             Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
             PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
             BufferedReader in = new BufferedReader((new InputStreamReader(socket.getInputStream())));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        ){
+            System.out.print("Enter username: ");
+            String username = stdIn.readLine();
+            out.println(username);
+
+            //starting a new thread listenting to messages from server
+            Thread serverListen = new Thread(() -> {
+                String serverMsg;
+                try{
+                    while((serverMsg = in.readLine()) != null){
+                        System.out.println(serverMsg);
+                    }
+                }catch(IOException e){
+                    System.out.println("Connection to server lost!");
+                }
+            });
+            serverListen.start();
+
             
 
         }catch(){
