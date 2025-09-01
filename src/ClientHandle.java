12b587b9410129f1bf76ff2c669c8c710be8bac0
Bongani X <velanibzuma@gmail.com>
2025-09-02 00:29:19 +0200

    -defined variables in client handle class for displaying communications(broadcasts)

--- src/ClientHandle.java
+++ src/ClientHandle.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandle {
     private final Socket clientSocket;
     private final AppServer server;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String user;
 }
