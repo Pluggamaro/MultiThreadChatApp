b0014f16863eb2207eb4e3342609e6df931536fa
Bongani X <velanibzuma@gmail.com>
2025-09-02 00:27:13 +0200

    -defining constructor for ClientHandle class -implementing new threads on new connections in the app handler class

--- src/AppServer.java
+++ src/AppServer.java
@@ -1,19 +1,32 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class AppServer {
     private static final int PORT = 9090;
 
-    private final Set<ClientHandle> clientHandler = new CopyOnWriteArraySet<>();
+    private final Set<ClientHandle> clientHandlers = new CopyOnWriteArraySet<>();
     
     public void start(){
         //informing of port number
         System.out.println("Server started on port: " + PORT);
         try(ServerSocket socket = new ServerSocket(PORT)){
             System.out.println("Server start success! Awaiting connections...");
-            
+            while(true){
+                //accepting new client
+                //blocks off others until success
+
+                Socket clientSocket = socket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                //creating new client handles starting on a new thread
+                ClientHandle clientHandler =  new ClientHandle(clientSocket, this);
+                clientHandlers.add(clientHandler);
+                new Thread(clientHandler).start();
+
+            }
 
         }catch(IOException w){
             System.err.println("Server error: " + w.getMessage());
--- src/ClientHandle.java
+++ src/ClientHandle.java
@@ -1,3 +1,6 @@
+import java.net.Socket;
+
 public class ClientHandle {
-    
+    private final Socket clientSocket;
+    private final AppServer server;
 }
